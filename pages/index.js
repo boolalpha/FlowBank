b1577ef9be86714e60df592a2e9685c7681a1242
IggyWright <iggy.wright81@gmail.com>
2022-03-20 21:38:44 -0400

    google analytics added

--- pages/index.js
+++ pages/index.js
@@ -1138,6 +1138,17 @@ export default function Home() {
     */
     const Controls = () => {
         return (
+            <Head>
+                <!-- Global site tag (gtag.js) - Google Analytics -->
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-S9WDY86NW3"></script>
+                <script>
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-S9WDY86NW3');
+                </script>
+            </Head>
             <div id="controllerContainer">
                 <div id="menuButton" onClick={showMenu}>
                     <img className="menuImg" src='/img/icons/menu.png' alt="menuImg"></img>
