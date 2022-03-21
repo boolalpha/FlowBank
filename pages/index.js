48181ec406c0e60d709d2eb29f3998c4db3dcdd3
IggyWright <iggy.wright81@gmail.com>
2022-03-20 21:45:03 -0400

    google analytics added

--- pages/index.js
+++ pages/index.js
@@ -1880,14 +1880,23 @@ export default function Home() {
     return (
         <div id="container">
             <Head>
-                <script async src="https://www.googletagmanager.com/gtag/js?id=G-S9WDY86NW3"></script>
-                <script>
+                {/* Global Site Tag (gtag.js) - Google Analytics */}
+                <script
+                    async
+                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                />
+                <script
+                    dangerouslySetInnerHTML={{
+                      __html: `
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
-
-                    gtag('config', 'G-S9WDY86NW3');
-                </script>
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                      page_path: window.location.pathname,
+                    });
+                    `,
+                    }}
+                />
             </Head>
             <div id="pixelMapContainer">
                 {pixelMap}
